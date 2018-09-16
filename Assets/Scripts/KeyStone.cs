8a51af63416e71e288332c85f6ce86efcde3763c
Yoshimune <seatangle.dash@gmail.com>
2018-09-16 21:02:07 +0900

    add project

---
+++ Assets/Scripts/KeyStone.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class KeyStone : MonoBehaviour {
+
+    [Header("PostProcessingをかけるShader")]
+    public Shader shader;
+
+    //Material Property
+    [Range(0, 5)]
+    public float UpperSide;
+    [Range(0, 5)]
+    public float LowerSide;
+
+    // PostProcessingシェーダーをもつMaterial
+    private Material _material;
+    private Material material
+    {
+        get
+        {
+            if(_material == null)
+            {
+                _material = new Material(shader);
+                // スクリプトから生成したマテリアルは保存しないようにする
+                _material.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return _material;
+        }
+    }
+
+    /// <summary>
+    /// Materiakの設定を行う
+    /// </summary>
+    private void SetMaterial()
+    {
+        // TODO materialの設定を行う
+        if (material.HasProperty("__width")) { material.SetFloat("__width", Screen.width); }
+        if (material.HasProperty("__height")) { material.SetFloat("__height", Screen.height); }
+        if (material.HasProperty("_upperSide")) { material.SetFloat("_upperSide", UpperSide); }
+        if(material.HasProperty("_lowerSide")) { material.SetFloat("_lowerSide", LowerSide); }
+    }
+
+	// Use this for initialization
+	void Start () {
+        if ( !SystemInfo.supportsImageEffects           // イメージエフェクトが使用可能か
+            || shader == null || shader.isSupported){   // shaderが使用可能か
+            // 自身のgameObjectを非活性にする
+            enabled = false;
+            return;
+        }
+
+        // Material の設定を行う
+        SetMaterial();
+	}
+
+    /// <summary>
+    /// レンダリング後に呼ばれるコールバック
+    /// </summary>
+    /// <param name="source">レンダリング結果</param>
+    /// <param name="destination">書き込み先</param>
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+#if UNITY_EDITOR
+        // エディター上ではインスペクタ上からの変更を反映させるためここでmaterialを再設定する
+        SetMaterial();
+#endif
+        // source に materialを適用してdestinationに書き込み
+        source.filterMode = FilterMode.Trilinear;
+        Graphics.Blit(source, destination, material);
+    }
+
+    /// <summary>
+    /// GameObjectが破棄される際に呼ばれるコールバック
+    /// </summary>
+    private void OnDestroy()
+    {
+        // materialを破棄
+        if (_material)
+        {
+            Destroy(_material);
+        }
+    }
+}
---
+++ Assets/Shaders/KeyStone.shader
@@ -0,0 +1,81 @@
+﻿Shader "KeyStone/KeyStone"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		[HideInspector] __width("__width", Float) = 1280
+		[HideInspector] __height("__hright", Float) = 720
+		_upperSide("Upper Side", Range(0.0, 2.0)) = 1.0
+		_lowerSide("Lower Side", Range(0.0, 2.0)) = 1.0
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+			uniform half __width;
+			uniform half __height;
+			uniform half _upperSide;
+			uniform half _lowerSide;
+
+			fixed4 frag (v2f_img i) : SV_Target
+			{
+				// uv.xをスケーリング
+				// uv.xを[0 - 1]から[-1 - 1]に変換する
+				half uvx = (i.uv.x * 2.0) - 1.0;
+
+				// xの拡大率を算出
+				// upperSide と _lowerSide を線形補間させる
+				half xScale = (_upperSide * i.uv.y) + (_lowerSide * (1.0 - i.uv.y));
+
+				// uv.xを算出
+				uvx /= xScale;
+
+				// uvを[-1 - 1]から[0 - 1]に変換する
+				uvx = (uvx + 1.0) * 0.5;
+
+
+
+				// uv.yをスケーリング
+				half ratio = _upperSide / _lowerSide;
+				half uvy = (i.uv.y / ((ratio - 1) * i.uv.y + 1)) * ratio;
+
+				half2 uv = half2(uvx, uvy);
+
+				fixed4 col = tex2D(_MainTex, uv);
+
+				//// Bilinearで拡大サンプリング
+				//// 座標を取得
+				//half2 scaleCoord = half2(__width * uv.x, __height * uv.y);
+
+				//// 元画像からのサンプリング座標を取得
+				////half2 sampleCoord = half2(floor(scaleCoord.x / xScale), floor(scaleCoord.y / xScale));
+				//half2 sampleCoord = half2(floor(scaleCoord.x), floor(scaleCoord.y));
+				//sampleCoord = half2(sampleCoord.x / __width, sampleCoord.y / __height);
+
+				//// 距離を求める
+				////half2 distance = half2(scaleCoord.x - sampleCoord.x, scaleCoord.y - sampleCoord.y);
+				//half distance = 1 / xScale;
+
+				//fixed4 col = tex2D(_MainTex, sampleCoord);
+				
+				/*fixed4 col = tex2D(_MainTex, uv) * (1.0 - distance.x) * (1.0 - distance.y)
+					+ tex2D(_MainTex, half2(uv.x + 1, uv.y)) * distance.x * (1.0 - distance.y)
+					+ tex2D(_MainTex, half2(uv.x, uv.y + 1)) * (1.0 - distance.x) * distance.y
+					+ tex2D(_MainTex, half2(uv.x + 1, uv.y + 1)) * distance.x * distance.y;*/
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
